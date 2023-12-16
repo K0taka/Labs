55e253e9e781f4f03f3c628ee487d3dc175008a0
K0taka <144228630+K0taka@users.noreply.github.com>
2023-12-16 13:32:20 +0500

    Add files via upload

--- Laba3/Task1/Program.cs
+++ Laba3/Task1/Program.cs
@@ -1,180 +1,93 @@
-﻿using System.Text.RegularExpressions;
+﻿using static System.Console;
+using static System.Math;
 
-namespace ver1
+namespace Task1
 {
     internal class Program
     {
-        static readonly Menu generateStringMenu = new(["Создать строку вручную", "Случайно сгенерировать строку", "Выход"]);
-        static readonly Menu postTaskMenu = new(["Вернуться на главную", "Выход"]);
+        //10 ВАРИАНТ
+        /*объявление значений заданных в ЛР, const тк они не будут меняться в ходе работы программы
+          double как среднее между float (недостаточно точности) и decimal (меньше производительности, высокая точность вычислений
+          этого типа дыннх игнорируется так как результат обрезается до 5 знаков после запятой)*/
+        const double accuracy = 0.0001; //точность для вычисления S(e) по усл. ЛР;
+        const double step = (1 - 0.1) / 10; //шаг с которым изменяется x в заданной функции
+        /*тип byte используется тк это наименьший по занимаемому объему целочисленный тип. Число повторений 20 помещяется
+          в границы данного типа 0...255*/
+        const byte repeat = (byte)20; //кол-во повторений для вычисления S(n) по усл. ЛР
+
         static void Main()
         {
-            while (true)
+            WriteLine("| Знач. X | Значение Y  | Значение S(n)  | Значение S(e) |");
+            for (double x = 0.1; x < 1; x += step) //x изменяется от 0.1 до 1 по условию ЛР, x - стандартное обозначение аргумента функции
             {
-                ChangeMenu(0);
+                double y, sumN, sumAcc; //объявление переменных для записи результатов вычислений
+                                        //y - стандартное обозначение значения функции
+                                        //sumN - обозначение для суммы вычисленной заданное число раз S(n)
+                                        //sumAcc - обозначение для суммы вычисленной до заданной точности члена суммы S(e)
+
+                x = Round(x, 2); //x округляем для избежания накопления ошибки вычислений последующего x
+                y = CalcY(x); //вычисляем y в соотв. функции
+                sumN = CalcSN(x); //вычисляем S(n) в соотв. функции (заданное кол-во повторений)
+                sumAcc = CalcSAcc(x); //вычисляем S(e) в соотв. функции (вычисляем пока не достигнем необходимой точности
+                                                //члена суммы)
+                WriteLine($"|X = {x,3:F2} | Y = {y,6:F5} | S(n) = {sumN,6:F5} | S(e) = {sumAcc,6:F5}|"); //вывод результата вычислений
+                                                                                                       //с точностью до 5 знаков
             }
+            ReadLine(); //избегаение закрытия консоли после вывода результата
         }
 
-        static void ChangeMenu(byte wishMenu)
+        /// <summary>
+        /// Функция для вычисления значения функции y по формуле y=e^cos(x) * cos(sin(x))
+        /// </summary>
+        /// <param name="x">Вещественное число</param>
+        /// <returns>Вещесвтенное значение указанной функции для переданного x</returns>
+        static double CalcY(double x)
         {
-            switch (wishMenu)
-            {
-                case 0:
-                    Console.Clear();
-                    generateStringMenu.ShowMenu();
-                    string wkStr = ChooseGeneration(generateStringMenu.SetUserAnswer());
-                    if (wkStr == "")
-                    {
-                        ChangeMenu(1);
-                        return;
-                    }
-                    string[] result = WorkingOnString(wkStr);
-                    Console.Clear();
-                    PrintResult(result, wkStr);
-                    ChangeMenu(1);
-                    break;
-                case 1:
-                    postTaskMenu.ShowMenu();
-                    postTaskMenu.SetUserAnswer();
-                    break;
-            }
+            double y; //переменная для хранения вычисленного значения, стандартное обозначение y для значения функции
+            y = Pow(E, Cos(x)) * Cos(Sin(x));
+            return y;
         }
 
-        static string ChooseGeneration(byte uAnsw)
+        /// <summary>
+        /// Функция для вычисления S(n), то есть суммы, полученной путем сложения repeat членов для указанного x 
+        /// </summary>
+        /// <param name="x">Вещественное число, значение аргумента функции</param>
+        /// <returns>Вещественное значение указанной суммы для переданного x</returns>
+        static double CalcSN(double x)
         {
-            string input = string.Empty;
-            switch(uAnsw)
+            double sumN = 1; //заранее записываем 1 член суммы заранее для удобства работы с циклом, переменная хранит искомую сумму
+            double lasRec = 1;//заранее записываем послюднюю найденную рекурретно часть члена (0! = 1), переменная хранит
+                                                                                          //рекуррентно вычисляемую часть
+
+            for (byte actualNum = 1; actualNum <= repeat; actualNum++) //вычисляем сумму для заданного числа повторений repeat,
+                                                                       //изначальное значение 1 (второй член суммы)
             {
-                case 1:
-                    input = ManualCreateString();
-                    break;
-                case 2:
-                    input = RandomCreateString();
-                    break;
+                lasRec *= actualNum; //находим текущую рекуррентную часть
+                sumN += Cos(actualNum * x) / lasRec; //обновляем сумму, первая числитель находим по формуле, знаменатель рекуррентно
             }
-            return input;
+            return sumN;
         }
 
-        static string[] WorkingOnString(string str)
+        /// <summary>
+        /// Функция для вычисления S(e), то есть суммы, полученной путем сложения стольких членов, пока не будет
+        /// вычислен такой, абсолютное значение которого не будет меньше, чем заданная точность accuracy
+        /// </summary>
+        /// <param name="x">Вещественное число, значение аргумента функции</param>
+        /// <returns>Вещественное значение указанной суммы для переданного x</returns>
+        static double CalcSAcc(double x)
         {
-            Regex symbs = new(@"^([a-z]|_)([a-z_0-9]|_)*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            Regex marks = new(@"(!|\u002e|\u003f|,|;|:)$");
-
-            str = str
-                    .Replace(".", ". ")
-                    .Replace("!", "! ")
-                    .Replace("?", "? ")
-                    .Replace(",", ", ")
-                    .Replace(";", "; ")
-                    .Replace(":", ": ")
-                    .Trim(); //не удаляет внутри строки пробелы. Значение минимума это слова языка и больше с такой же длиной нет
-
-            string[] words = str.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            List<string> correctWords = [];
-            string[] result = null!;
-            string[] keywords = ["abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue",
-                                 "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "true",
-                                 "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is",
-                                 "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override", "params", "private", "protected",
-                                 "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct",
-                                 "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual",
-                                 "void", "volatile", "while", "add", "and", "alias", "ascending", "args", "async", "await", "by", "descending", "dynamic",
-                                 "equals", "file", "from", "get", "global", "group", "init", "into", "join", "let", "managed", "nameof", "nint", "not", "notnull",
-                                 "nuint", "on", "or", "orderby", "partial", "record", "remove", "required", "scoped", "select", "set", "unmanaged",
-                                 "value", "var", "when", "where", "with", "yield"];
-            for (int index = 0; index < words.Length; index++)
+            double lastRec = 1; //заранее записываем послюднюю найденную рекурретно часть члена (0! = 1), переменная хранит
+                                //рекуррентно вычисляемую часть
+            double sumAcc = 1; //переменная хранит вычисляемую сумму, изначально = 1, так как в нее уже записан нулевой член
+            double curElem = 1; //переменная хранит элемент суммы для сравнения с точностью, изначально равен нулевому члену
+            
+            for (int actualNum = 1; Abs(curElem) > accuracy; actualNum++) //вычисляем искомую сумму, пока элемент не достигнет заданной точности
             {
-                string word = words[index];
-                if (marks.IsMatch(word))
-                    word = word
-                        .Replace("?", "")
-                        .Replace("!", "")
-                        .Replace(".", "")
-                        .Replace(",", "")
-                        .Replace(";", "")
-                        .Replace(":", "");
-                if (symbs.IsMatch(word))
-                    correctWords.Add(word);
+                lastRec *= actualNum; //вычисляем текующую рекуррентную часть выражения
+                curElem = Cos(x * actualNum) / lastRec; //вычисляем текущий элемент
+                sumAcc += curElem; //добавляем его к сумме
             }
-            correctWords = correctWords.Except(keywords).ToList();
-            if (correctWords.Count > 0)
-            {
-                int minLen = Enumerable.Min(correctWords.Select(x => x.Length));
-                result = correctWords.Distinct().Where(x => x.Length == minLen).ToArray();
-            }
-            return result!;
-        }
-
-        static string ManualCreateString()
-        {
-            bool isCorrect = false;
-            string input;
-            Console.Clear();
-            Console.Write("Укажите вашу строку >>> ");
-            do
-            {
-                input = Console.ReadLine()!;
-                try
-                {
-                    StringCheck(input);
-                    isCorrect = true;
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.Write(ex.Message);
-                } 
-            } while (!isCorrect);
-            return input;
-        }
-
-        static string RandomCreateString()
-        {
-            string[] tests = File.ReadAllLines("D:\\University\\dz\\K0taka\\Vikentieva\\L6\\ver1\\Tests.txt");
-            Random rand = new();
-            string input = tests[rand.Next(tests.Length)];
-            try
-            {
-                StringCheck(input);
-                return input;
-            }
-            catch (Exception ex)
-            {
-                Console.Clear();
-                Console.WriteLine($"Случайная строка следующего содержания\n{input}\nне удовлетворяет условиям вводимой строки");
-                Console.WriteLine($"Расшифровка ошибки: {ex.Message.Remove(ex.Message.IndexOf('.'))}");
-            }
-            return "";
-        }
-
-        static void PrintResult(string[] result, string start)
-        {
-            if (result == null || result.Length == 0)
-            {
-                Console.WriteLine("В строке нет индентефикаторов!");
-                return;
-            }
-            Console.WriteLine($"В введенной строке {start} были найдены следующие идентефикаторы:");
-            string toPrint = string.Join(" | ", result);
-            Console.WriteLine(toPrint);
-        }
-
-        static void StringCheck(string str)
-        {
-            Regex pCheck = new(@"^([a-z_0-9]|_)([a-z_0-9]|_|!|\u003f|\u002e|,|;|:|\u0020)*(\.|\?|!)$", RegexOptions.IgnoreCase | RegexOptions.Compiled); //оканчивается зн. преп. и не нач с него
-            Regex sFormat = new(@"^([a-z_0-9]|_|!|\u003f|\u002e|,|;|:|\u0020)+$", RegexOptions.IgnoreCase | RegexOptions.Compiled); //содержание символов
-            Regex dCheck = new(@"(!|\u003f|\u002e|,|;|:|\u0020)(!|\u003f|\u002e|,|;|:)+", RegexOptions.IgnoreCase | RegexOptions.Compiled);//несколько спец. символов подряд
-            Regex dSpace = new(@"\u0020{2}");
-            Regex onlyPoint = new(@"^(\.|\?|!)$");
-            if (str == null || str.Length == 0)
-                throw new ArgumentException("Выполнен пусстой ввод. Повторите ввод предложений >>> ");
-            if (!sFormat.IsMatch(str))
-                throw new ArgumentException("В строке присутствуют символы, не относящиеся к поддерживаемому алфавиту. Повторите ввод предложений >>> ");
-            if (!pCheck.IsMatch(str))
-                throw new ArgumentException("Ошибка в знаках препинания. Повторите ввод предложений >>> ");
-            if (dCheck.IsMatch(str) || dSpace.IsMatch(str))
-                throw new ArgumentException("Дублирование специальных знаков. Повторите ввод предложений >>> ");
-            if (onlyPoint.IsMatch(str))
-                throw new ArgumentException($"В строке нет иных символов, кроме {str}. Повторите ввод предложений >>> ");
-            return;
+            return sumAcc;
         }
     }
 }
