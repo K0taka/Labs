4cdecbadb2e9bb5330710b8df6f94c1be48fc2e1
K0taka <144228630+K0taka@users.noreply.github.com>
2024-02-18 02:14:44 +0500

    Part 2 Requests created

---
+++ L10/Lab10Lib/Lab10Lib/Requests.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lab10Lib
+{
+    public class Requests
+    {
+        public enum Request
+        {
+            EnableMultButtonText,
+            ExistTextWithExistHint,
+            AllElementsAtXPos
+        }
+
+        public static string[] SendRequest(ControlElement[] array, Request req)
+        {
+            return req switch
+            {
+                Request.EnableMultButtonText => EnableMultButtonText(array),
+                Request.ExistTextWithExistHint => ExistTextWithExistHint(array),
+                Request.AllElementsAtXPos => AllElementsAtXPos(array),
+                _ => throw new KeyNotFoundException("Request do not exist")
+            };
+        }
+
+        private static string[] EnableMultButtonText(ControlElement[] array)
+        {
+            List<string> buttonsText = [];
+            foreach (ControlElement element in array)
+            {
+                if (element is not MultButton btn)
+                    continue;
+                if (btn.IsEnabled)
+                    buttonsText.Add(btn.Text);
+            }
+            return buttonsText.ToArray();
+        }
+
+        private static string[] ExistTextWithExistHint(ControlElement[] array)
+        {
+            Regex NotEmpty = new(@"\S+");
+            List<string> texts = [];
+            foreach(ControlElement element in array)
+            {
+                if (element is not TextField textField)
+                    continue;
+                if(textField.Hint != null && NotEmpty.IsMatch(textField.Hint) && textField.Text != null && NotEmpty.IsMatch(textField.Text))
+                    texts.Add(textField.Text);
+            }
+            return texts.ToArray();
+        }
+
+        private static string[] AllElementsAtXPos(ControlElement[] array)
+        {
+
+        }
+    }
+}
