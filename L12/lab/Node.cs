79ada1d7710d73d7d96d40e4301b8cf82f1dc8cb
K0taka <144228630+K0taka@users.noreply.github.com>
2024-04-09 15:48:40 +0500

    Comments

--- L12/lab/Node.cs
+++ L12/lab/Node.cs
@@ -2,12 +2,27 @@
 {
     public class Node<T>: IDisposable where T: IDisposable
     {
+        /// <summary>
+        /// Автосвойство для хранения объекта; null не предусмотрен
+        /// </summary>
         public T Data { get; set; }
 
+        /// <summary>
+        /// Ссылка на следующий элементъ
+        /// </summary>
         public Node<T>? Next { get; set; }
 
+        /// <summary>
+        /// Ссылка на предыдущий элемент
+        /// </summary>
         public Node<T>? Previous { get; set; }
 
+        /// <summary>
+        /// Конструктор для создания узла с информацией и ссылками на следующий и предыдущий элементы
+        /// </summary>
+        /// <param name="data">Объект в узле</param>
+        /// <param name="next">Ссылка на следующий</param>
+        /// <param name="previous">Ссылка на предыдущий</param>
         public Node(T data, Node<T>? next = null, Node<T>? previous = null)
         {
             Data = data;
@@ -15,11 +30,18 @@
             Previous = previous;
         }
 
+        /// <summary>
+        /// Информация о содержащимся объекте в строковом представлении
+        /// </summary>
+        /// <returns>Строка с информацией об объекте</returns>
         public override string? ToString()
         {
             return Data != null ? Data.ToString() : "";
         }
 
+        /// <summary>
+        /// Уничтожение узла
+        /// </summary>
         public void Dispose()
         {
             Previous = null;
