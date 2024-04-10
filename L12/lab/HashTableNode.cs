f7cb77ac214a8e54a4affdc571459c98b82119d6
K0taka <144228630+K0taka@users.noreply.github.com>
2024-04-10 16:33:46 +0500

    Now has chain len

--- L12/lab/HashTableNode.cs
+++ L12/lab/HashTableNode.cs
@@ -2,18 +2,22 @@
 {
     public class HashTableNode<T> : IDisposable where T : IDisposable
     {
+        public int ChainLen { get; set; }
+
         public T Data { get; set; }
 
         public HashTableNode<T>? Next { get; set; }
 
-        public HashTableNode(T data, HashTableNode<T>? next = null)
+        public HashTableNode(T data, HashTableNode<T>? next = null, int chainLen = 1)
         {
             Data = data;
             Next = next;
+            ChainLen = chainLen;
         }
 
         public void Dispose()
         {
+            ChainLen = 0;
             Data.Dispose();
             Next = null;
             GC.SuppressFinalize(this);
