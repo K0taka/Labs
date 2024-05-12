8293321f6a4ac62b1411c5770c49e77ecb82cd31
K0taka <144228630+K0taka@users.noreply.github.com>
2024-05-13 02:14:39 +0500

    Comments

--- L12/lab/HashTableChain.cs
+++ L12/lab/HashTableChain.cs
@@ -4,16 +4,30 @@ namespace lab
 {
     public class HashTableChain<TKey, TValue> : IEnumerable<HashTableNode<TKey,TValue>> where TValue : ICloneable where TKey : notnull, ICloneable
     {
+        /// <summary>
+        /// Голова списка
+        /// </summary>
         HashTableNode<TKey, TValue>? Head;
 
+        /// <summary>
+        /// Количество элементов в списке
+        /// </summary>
         public int Count { get; private set; }
 
+        /// <summary>
+        /// Конструктор пустой цепочки
+        /// </summary>
         public HashTableChain()
         {
             Head = null;
             Count = 0;
         }
 
+        /// <summary>
+        /// Содержание ключа в цепочке
+        /// </summary>
+        /// <param name="key">Ключ для проверки</param>
+        /// <returns>true если содержится, иначе false</returns>
         public bool Contains(TKey key)
         {
             var curr = Head;
@@ -26,6 +40,12 @@ namespace lab
             return false;
         }
 
+        /// <summary>
+        /// Добавление в цепочку ключа и значения
+        /// </summary>
+        /// <param name="key">Ключ для добавления</param>
+        /// <param name="value">Значение для добавления</param>
+        /// <exception cref="ArgumentException">такой ключ уже добавлен</exception>
         public void Add(TKey key, TValue value)
         {
             if (Contains(key))
@@ -34,8 +54,17 @@ namespace lab
             Count += 1;
         }
 
+        /// <summary>
+        /// Очистка цепочки
+        /// </summary>
         public void Clear() { Head = null; Count = 0; }
 
+        /// <summary>
+        /// Удаление элемента из цепочки по ключу
+        /// </summary>
+        /// <param name="key">Ключ для удаления</param>
+        /// <exception cref="InvalidOperationException">Цепочка пуста</exception>
+        /// <exception cref="KeyNotFoundException">Ключ не найден</exception>
         public void Remove(TKey key)
         {
             var curr = Head ?? throw new InvalidOperationException();
@@ -56,6 +85,10 @@ namespace lab
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
+        /// <summary>
+        /// Полуение нумератора для цепочки
+        /// </summary>
+        /// <returns>Нумератор из узлов в цепочке</returns>
         public IEnumerator<HashTableNode<TKey, TValue>> GetEnumerator()
         {
             var curr = Head;
@@ -67,6 +100,12 @@ namespace lab
             yield break;
         }
 
+        /// <summary>
+        /// Индексатор для получения значения по ключу
+        /// </summary>
+        /// <param name="key">Ключ для получения значения</param>
+        /// <returns>Значения у соотв. ключа</returns>
+        /// <exception cref="KeyNotFoundException">Ключ не найден</exception>
         public TValue this[TKey key]
         {
             get
--- L12/lab/HashTableNode.cs
+++ L12/lab/HashTableNode.cs
@@ -3,12 +3,27 @@
     public class HashTableNode<TKey, TValue> where TKey: notnull
     {
 
+        /// <summary>
+        /// Автосвойство для ключа
+        /// </summary>
         public TKey Key { get; set; }
 
+        /// <summary>
+        /// Автосвойство для значения
+        /// </summary>
         public TValue Data { get; set; }
 
+        /// <summary>
+        /// Следующий узел в цепочке
+        /// </summary>
         public HashTableNode<TKey, TValue>? Next { get; set; }
 
+        /// <summary>
+        /// Конструктор для узла
+        /// </summary>
+        /// <param name="key">Ключ для добавления</param>
+        /// <param name="data">Значение для добавлеия</param>
+        /// <param name="next">Следующий элемент в цепочке</param>
         public HashTableNode(TKey key, TValue data, HashTableNode<TKey, TValue>? next = null)
         {
             Key = key;
@@ -16,6 +31,10 @@
             Next = next;
         }
 
+        /// <summary>
+        /// хэш-код узла
+        /// </summary>
+        /// <returns>Хэш-код сохраненного ключа</returns>
         public override int GetHashCode()
         {
             return Key.GetHashCode();
