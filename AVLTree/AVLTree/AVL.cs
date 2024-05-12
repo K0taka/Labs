9262199603b684465536572807215722c88727df
K0taka <144228630+K0taka@users.noreply.github.com>
2024-05-13 01:56:12 +0500

    Comments

--- AVLTree/AVLTree/AVL.cs
+++ AVLTree/AVLTree/AVL.cs
@@ -8,14 +8,26 @@ namespace AVLTree
     {
         private Node<TKey, TValue>? root;
 
+        /// <summary>
+        /// Свойство для получения корня дерева
+        /// </summary>
         public Node<TKey, TValue>? Root => root;
 
         private int count;
 
+        /// <summary>
+        /// Свойство для получения количества элементов
+        /// </summary>
         public int Count => count;
 
+        /// <summary>
+        /// Автосвойства для установки дерева в режим "только для чтения"
+        /// </summary>
         public bool IsReadOnly { get; set; }
 
+        /// <summary>
+        /// Свойство для получения всех ключей в дереве
+        /// </summary>
         public ICollection<TKey> Keys
         {
             get
@@ -31,6 +43,9 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Свойство для получения всех значений в дереве
+        /// </summary>
         public ICollection<TValue> Values
         {
             get
@@ -46,8 +61,15 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Конструктор пустого дерева
+        /// </summary>
         public AVL() { }
 
+        /// <summary>
+        /// Конструктор копирования из другого дерева
+        /// </summary>
+        /// <param name="tree">Дерево, из которого требуется копировать элементы</param>
         public AVL(AVL<TKey, TValue> tree)
         {
             if (typeof(TKey).GetInterfaces().Contains(typeof(ICloneable)))
@@ -69,8 +91,19 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Функция для добавления ключа и значения в дерево
+        /// </summary>
+        /// <param name="key">Ключ, должен быть уникальный</param>
+        /// <param name="value">Значение соответствующее этому ключу</param>
         public void Add(TKey key, TValue value) { try { Add(new KeyValuePair<TKey, TValue>(key, value)); } catch { throw; } }
 
+        /// <summary>
+        /// Функция для добавления ключа и значения в дерево
+        /// </summary>
+        /// <param name="item">Пара ключ-значение для добавления</param>
+        /// <exception cref="NotSupportedException">Дерево сейчас только для чтения</exception>
+        /// <exception cref="ArgumentNullException">Ключ был null</exception>
         public void Add(KeyValuePair<TKey, TValue> item)
         {
             if (IsReadOnly)
@@ -81,6 +114,12 @@ namespace AVLTree
             try { Add(item, root); } catch { throw; }
         }
 
+        /// <summary>
+        /// Рекурсивная функция для добавления в дерево и последующей балансировки оного
+        /// </summary>
+        /// <param name="item">Пара ключ-значение для добавления</param>
+        /// <param name="node">Текущий узел</param>
+        /// <exception cref="ArgumentException">Такое ключ уже существует</exception>
         private void Add(KeyValuePair<TKey, TValue> item, Node<TKey, TValue> node)
         {
             switch(node.Entry.Key.CompareTo(item.Key))
@@ -111,6 +150,10 @@ namespace AVLTree
             Balance(node);
         }
 
+        /// <summary>
+        /// Балансирует поддерево от указанного узла
+        /// </summary>
+        /// <param name="node"></param>
         private static void Balance(Node<TKey, TValue> node)
         {
             int balance = GetBalance(node);
@@ -128,45 +171,71 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Осуществляет правый поворот дерева
+        /// </summary>
+        /// <param name="node">Узел относительно которого совершается поворот</param>
         private static void RightRotate(Node<TKey, TValue> node)
         {
             Swap(node, node.Left);
-            Node<TKey, TValue>? buffer = node.Right;
+            Node<TKey, TValue>? rightNode = node.Right;
             node.Right = node.Left;
             node.Left = node.Right.Left;
             node.Right.Left = node.Right.Right;
-            node.Right.Right = buffer;
+            node.Right.Right = rightNode;
             UpdateHight(node.Right);
             UpdateHight(node);
         }
 
+        /// <summary>
+        /// Осуществляет левый поворот дерева
+        /// </summary>
+        /// <param name="node">Узел относительно которого совершается поворот</param>
         private static void LeftRotate(Node<TKey, TValue> node)
         {
             Swap(node, node.Right);
-            Node<TKey, TValue>? buffer = node.Left;
+            Node<TKey, TValue>? leftNode = node.Left;
             node.Left = node.Right;
             node.Right = node.Left.Right;
             node.Left.Right = node.Left.Left;
-            node.Left.Left = buffer;
+            node.Left.Left = leftNode;
             UpdateHight(node.Left);
             UpdateHight(node);
         }
 
+        /// <summary>
+        /// Функция для обновления высоты узла
+        /// </summary>
+        /// <param name="node">Узел для обновления</param>
         private static void UpdateHight(Node<TKey, TValue> node)
         {
             node.Hight = Math.Max(node.Left == null ? -1 : node.Left.Hight, node.Right == null ? -1 : node.Right.Hight) + 1;
         }
 
+        /// <summary>
+        /// Функция для получения коэф. баланса у указанного узла
+        /// </summary>
+        /// <param name="node">Узел для получения баланса</param>
+        /// <returns>Число - значение баланса</returns>
         private static int GetBalance(Node<TKey, TValue> node)
         {
             return (node.Right == null ? -1 : node.Right.Hight) - (node.Left == null ? -1 : node.Left.Hight);
         }
         
+        /// <summary>
+        /// Меняет записи двух узлов
+        /// </summary>
+        /// <param name="firstNode">Первый узел для замены</param>
+        /// <param name="secondNode">Второй узел для замены</param>
         private static void Swap(Node<TKey, TValue> firstNode, Node<TKey, TValue> secondNode)
         {
             (firstNode.Entry, secondNode.Entry) = (secondNode.Entry, firstNode.Entry);
         }
 
+        /// <summary>
+        /// Функция для очистки дерева
+        /// </summary>
+        /// <exception cref="NotSupportedException"></exception>
         public void Clear()
         {
             if (IsReadOnly)
@@ -175,6 +244,11 @@ namespace AVLTree
             count = 0;
         }
 
+        /// <summary>
+        /// Функция для проверки наличия конкретной пары в дереве
+        /// </summary>
+        /// <param name="item">Пара, наличие которой проверяется</param>
+        /// <returns>true если пара существует в дереве, иначе false</returns>
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
             var current = root;
@@ -203,11 +277,20 @@ namespace AVLTree
             return GetEnumerator();
         }
 
+        /// <summary>
+        /// Получение энумератора для дерева
+        /// </summary>
+        /// <returns>Нумератор в отсортированном по удаленности от точки (0; 0)</returns>
         public IEnumerator<KeyValuePair<TKey,TValue>> GetEnumerator()
         {
             return InOrderEmumerator(root).GetEnumerator();
         }
 
+        /// <summary>
+        /// Функция формирующая рекурсивно отсортированный по удаленности (по возрастанию) дерево
+        /// </summary>
+        /// <param name="node">Текущий узел</param>
+        /// <returns>Созданный энумератор</returns>
         private static IEnumerable<KeyValuePair<TKey, TValue>> InOrderEmumerator(Node<TKey, TValue> node)
         {
             if (node != null)
@@ -224,6 +307,10 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Обход дерева в ширину
+        /// </summary>
+        /// <returns>Созданый нумератор</returns>
         public IEnumerable<KeyValuePair<TKey,TValue>> InWideEnumerator()
         {
             if (root == null)
@@ -245,6 +332,13 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Удаление пары ключ-значение из дерева
+        /// </summary>
+        /// <param name="item">Пара для удаления</param>
+        /// <returns>true если удалено, иначе false</returns>
+        /// <exception cref="NotSupportedException">Доступно только для чтения</exception>
+        /// <exception cref="ArgumentNullException">Ключ был null</exception>
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
             if (IsReadOnly)
@@ -264,6 +358,13 @@ namespace AVLTree
             return false;
         }
 
+        /// <summary>
+        /// Удаление пары из дерева по ключу
+        /// </summary>
+        /// <param name="key">Ключ, по которому производится удаление</param>
+        /// <returns>true если удалено, иначе false</returns>
+        /// <exception cref="NotSupportedException">Доступно только для чтения</exception>
+        /// <exception cref="ArgumentNullException">Ключ был null</exception>
         public bool Remove(TKey key)
         {
             if (IsReadOnly)
@@ -277,6 +378,13 @@ namespace AVLTree
             return isDeleted;
         }
 
+        /// <summary>
+        /// Рекурсивная функция удаления по ключу из дерева
+        /// </summary>
+        /// <param name="node">Текущий узел</param>
+        /// <param name="key">Ключ по которому производится удаление</param>
+        /// <param name="isDeleted">Флаг об успешности удаления</param>
+        /// <returns>Измененный узел</returns>
         private Node<TKey, TValue>? Remove(Node<TKey, TValue>? node, TKey key, ref bool isDeleted)
         {
             if (node == null)
@@ -314,11 +422,24 @@ namespace AVLTree
             return node;
         }
 
+        /// <summary>
+        /// Функция для получения минимального узла в поддереве
+        /// </summary>
+        /// <param name="node">Узел начала поддерева</param>
+        /// <returns>Минимальный узел</returns>
         private static Node<TKey, TValue> GetMin(Node<TKey, TValue> node)
         {
             return node.Left == null ? node : GetMin(node.Left);
         }
 
+        /// <summary>
+        /// Функция для копирования элементов в дереве в массив в порядке возрастания расстояния от начала
+        /// </summary>
+        /// <param name="array">массив для копирования</param>
+        /// <param name="index">позиция начала</param>
+        /// <exception cref="ArgumentNullException">список был null</exception>
+        /// <exception cref="ArgumentOutOfRangeException">index был меньше 0</exception>
+        /// <exception cref="ArgumentException">недостаточно места в массиве</exception>
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int index)
         {
             if (array == null)
@@ -335,6 +456,14 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Индексатор для получения значения по ключу
+        /// </summary>
+        /// <param name="key">Ключ по которому необходимо получить значение</param>
+        /// <returns>Значение находящаемся по заданному ключу в дереве</returns>
+        /// <exception cref="ArgumentNullException">Ключ был null</exception>
+        /// <exception cref="KeyNotFoundException">Ключ не найден</exception>
+        /// <exception cref="NotSupportedException">Доступно только для чтения</exception>
         public TValue this[TKey key]
         {
             get
@@ -385,6 +514,12 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Проверка на наличие ключа в дереве
+        /// </summary>
+        /// <param name="key">Ключ для проверки</param>
+        /// <returns>true если ключ содержится, иначе false</returns>
+        /// <exception cref="ArgumentNullException">ключ был null</exception>
         public bool ContainsKey(TKey key)
         {
             if (key == null)
@@ -407,6 +542,12 @@ namespace AVLTree
             return false;
         }
 
+        /// <summary>
+        /// Функция для получения значения через индексатор, при ошибке вернет false
+        /// </summary>
+        /// <param name="key">Ключ для получения значения</param>
+        /// <param name="value">Значение по указанному ключу</param>
+        /// <returns>true в случае успеха, иначе false</returns>
         public bool TryGetValue(TKey key, out TValue value)
         {
             try
@@ -421,11 +562,20 @@ namespace AVLTree
             }
         }
 
+        /// <summary>
+        /// Глубокое копирование дерева
+        /// </summary>
+        /// <returns>Новое дерево</returns>
         public object Clone()
         {
             return new AVL<TKey, TValue>(this);
         }
 
+        /// <summary>
+        /// Проверка деревьев на равенство
+        /// </summary>
+        /// <param name="obj">для сравнения</param>
+        /// <returns>true если ранвы, иначе false</returns>
         public override bool Equals(object? obj)
         {
             if (obj is not AVL<TKey, TValue> tree)
@@ -443,11 +593,20 @@ namespace AVLTree
             return true;
         }
 
+        /// <summary>
+        /// Поверхностная копия объекта
+        /// </summary>
+        /// <returns>новый объект - поверхностная копия текущего дерева</returns>
         public object ShallowCopy()
         {
             return this.MemberwiseClone();
         }
 
+        /// <summary>
+        /// Проверка на наличие значения в дереве
+        /// </summary>
+        /// <param name="value">значение для поиска</param>
+        /// <returns>true если найдено, иначе false</returns>
         public bool ContainsValue(TValue value)
         {
             foreach( var item in this)
