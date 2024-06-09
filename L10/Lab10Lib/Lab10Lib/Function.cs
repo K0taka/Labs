52a7f09d57780cfc1c37753cdd60b6da1b3b9b2e
K0taka <144228630+K0taka@users.noreply.github.com>
2024-06-09 20:28:46 +0500

    new class for L14
    Fuction class

---
+++ L10/Lab10Lib/Lab10Lib/Function.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Lab10Lib
+{
+    public class Function
+    {
+        uint connectedID;
+        readonly string description;
+
+        public uint ConnectedID
+        {
+            get { return connectedID; }
+            set { connectedID = value; }
+        }
+
+        public string Description => description;
+
+        public Function(uint ID, string description)
+        {
+            connectedID = ID;
+            this.description = description; 
+        }
+    }
+}
